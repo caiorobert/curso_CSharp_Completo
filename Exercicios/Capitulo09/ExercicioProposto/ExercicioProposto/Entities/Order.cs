fdb6e9304a419857cc2f382f34d2e578fd107e07
Caio Robert <caio.robert@live.com>
2021-03-24 22:54:51 -0300

    Capitulo 09 - Enumberação e composição + Exercício, muito bom!

---
+++ Exercicios/Capitulo09/ExercicioProposto/ExercicioProposto/Entities/Order.cs
@@ -0,0 +1,65 @@
+﻿using ExercicioProposto.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ExercicioProposto.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+
+        }
+
+        public Order(OrderStatus status, Client client)
+        {
+            Moment = DateTime.Now;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem orderItem)
+        {
+            OrderItems.Add(orderItem);
+        }
+
+        public void RemoveItem(OrderItem orderItem)
+        {
+            OrderItems.Remove(orderItem);
+        }
+
+        public double Total()
+        {
+            double total = 0.0;
+
+            foreach (OrderItem item in OrderItems)
+            {
+                total += item.SubTotal();
+            }
+
+            return total;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Order moment: {Moment.ToString("dd/MM/yyyy HH:mm:ss")}");
+            sb.AppendLine($"Order status: {Status}");
+            sb.AppendLine($"Client: {Client}");
+            sb.AppendLine("Order items:");
+            foreach (OrderItem item in OrderItems)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            sb.AppendLine($"Total price: {Total().ToString("F2", CultureInfo.InvariantCulture)}");
+            return sb.ToString();
+        }
+    }
+}
---
+++ Exercicios/Capitulo09/ExercicioProposto/ExercicioProposto/Program.cs
@@ -0,0 +1,53 @@
+﻿using ExercicioProposto.Entities;
+using ExercicioProposto.Entities.Enums;
+using System;
+using System.Globalization;
+
+namespace ExercicioProposto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dados do cliente
+            Console.WriteLine("Enter cliente data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            Client client = new Client(name, email, birthDate);
+
+
+            //Dados do pedido
+            Console.WriteLine("Enter order data:");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Order order = new Order(status, client);
+
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data:");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Product product = new Product(productName, price);
+
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                OrderItem orderItem = new OrderItem(quantity, price, product);
+                order.AddItem(orderItem);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMARY:");
+            Console.WriteLine(order);
+        }
+    }
+}
